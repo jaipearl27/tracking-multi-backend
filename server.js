d51ce52c6fdeec3051b2dabdd4182bb7914142e7
jaipearl27 <jaix2793@gmail.com>
2025-04-08 12:06:42 +0530

    updates

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(cors({
         'http://localhost:3001',
         'http://localhost:5173',
         'http://localhost:5174',
+        'https://tracking-mern.vercel.app'
     ],
     methods: [
         'GET',
