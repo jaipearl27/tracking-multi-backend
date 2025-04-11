837e144293a18b211374b085e8ba416cb31be350
jaipearl27 <jaix2793@gmail.com>
2025-04-11 11:12:01 +0530

    cart decrease and delete bug resolved

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -2,7 +2,7 @@ const isProduction = process.env.ENVIRONMENT === "production";
 
 export const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: isProduction,
+  secure: true, // isProduction,
   sameSite: isProduction ? "lax" : "None", // "none" for dev to allow cross-site cookies
   path: "/"
 };
